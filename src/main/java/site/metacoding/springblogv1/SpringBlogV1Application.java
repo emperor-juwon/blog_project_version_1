8ece6a6aa6cd1430cba3702e343ae9ab9de5c572
fwangjuwon <fwangjuwon@naver.com>
2022-05-02 19:55:32 +0900

    README 업로드

---
+++ src/main/java/site/metacoding/springblogv1/SpringBlogV1Application.java
@@ -0,0 +1,13 @@
+package site.metacoding.springblogv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBlogV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBlogV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/site/metacoding/springblogv1/SpringBlogV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package site.metacoding.springblogv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBlogV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
