9d36e07b161db4e01147fbe5dcfe909e36300204
emperor-juwon <fwangjuwon@gmail.com>
2022-05-31 17:34:01 +0900

    글쓰기 완료 리스트 뿌리기 미완료

--- src/main/java/site/metacoding/springblogv1/web/PostController.java
+++ src/main/java/site/metacoding/springblogv1/web/PostController.java
@@ -1,5 +1,7 @@
 package site.metacoding.springblogv1.web;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,11 +9,25 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 
+import lombok.RequiredArgsConstructor;
+import site.metacoding.springblogv1.domain.post.Post;
+import site.metacoding.springblogv1.domain.post.PostRepository;
+import site.metacoding.springblogv1.domain.user.User;
+
+@RequiredArgsConstructor
 @Controller
 public class PostController {
+
+    private final  HttpSession session;
+    private final PostRepository postRepository;
     
         @GetMapping("/s/post/writeForm")
         public String writeForm() {
+
+            //인증 체크
+            if (session.getAttribute("principal") == null) {
+                return "redirect:/loginForm";
+            }
             return "post/writeForm";
         }
     
@@ -41,7 +57,19 @@ public class PostController {
         }
 
         @PostMapping("/s/post")
-        public String write() {
+        public String write(Post post) {
+
+            User principal = (User)session.getAttribute("principal");
+
+            //인증체크
+            if (session.getAttribute("principal") == null) {
+                return "redirect:/loginForm";
+            }
+
+            post.setUser(principal);
+
+            postRepository.save(post);
+
             return "redirect:/";
         }
     }
